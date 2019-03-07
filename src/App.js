977026714177a0a2d491e53cbcb351456e281e96
Zack Bunn <zrb6578@gmail.com>
2019-03-06 20:07:50 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import Split from './composition/Split';
 import './App.css';
+// import Tooltip from './composition/Tooltip';
+// import Messages from './Messages';
+// import TheDate from './state/TheDate';
+// import Counter from './state/Counter'
+import HelloWorld from './state-drills/HelloWorld'
 
+// make 2 tooltips here and another inside the App directly
+// const firstTooltip = (
+//   <Tooltip color='hotpink' message='tooltip message'>
+//    ipsum
+//   </Tooltip>
+// )
+// const secondTooltip = (
+//   <Tooltip color='#126BCC' message='another tooltip message'>
+//     officiis
+//   </Tooltip>
+// )
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      
+      // <main className='App'>
+        
+      //   <Split className='left' flexBasis='2'>
+      //     This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
+      //     <Tooltip message='one more tooltip message'>
+      //       Necessitatibus?
+      //     </Tooltip>
+      //   </Split>
+      //   <Split className='right'>
+      //     This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
+      //   </Split> 
+      // </main>
+        // <div className="App">
+        //   <h1>YOUR APPLICATION NAME!</h1>
+        //   <Messages name="Messages" unread={0}/>
+        //   <Messages name="Notifications" unread={10}/>  
+        // </div>
+        // <TheDate>
+        //   <div></div>
+        // </TheDate>
+
+        // <Counter count={123}>
+        //   <div></div>
+        // </Counter>
+        <HelloWorld>
+          <div></div>
+        </HelloWorld>
+
+
     );
   }
-}
-
+};
 export default App;
